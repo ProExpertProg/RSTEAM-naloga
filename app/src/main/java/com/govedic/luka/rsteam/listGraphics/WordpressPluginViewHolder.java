1f17122bc8e8bbb096e9e08598c36e103ae4ff9e
Luka Govedic <luka.govedic@druga.si>
2018-04-12 20:39:34 +0200

    RecyclerView and graphics

---
+++ app/src/main/java/com/govedic/luka/rsteam/listGraphics/WordpressPluginViewHolder.java
@@ -0,0 +1,14 @@
+package com.govedic.luka.rsteam.listGraphics;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class WordpressPluginViewHolder extends RecyclerView.ViewHolder {
+
+    public View mItemView;
+
+    public WordpressPluginViewHolder(View itemView) {
+        super(itemView);
+        mItemView = itemView;
+    }
+}
--- app/src/main/java/com/govedic/luka/rsteam/listGraphics/WordpressPluginsAdapter.java
+++ app/src/main/java/com/govedic/luka/rsteam/listGraphics/WordpressPluginsAdapter.java
@@ -2,8 +2,12 @@ package com.govedic.luka.rsteam.listGraphics;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.govedic.luka.rsteam.R;
 import com.govedic.luka.rsteam.wordpress.WordpressPlugin;
 
 public class WordpressPluginsAdapter extends RecyclerView.Adapter {
@@ -16,7 +20,9 @@ public class WordpressPluginsAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.plugin_item_view, parent, false);
+        return new WordpressPluginViewHolder(v);
     }
 
     @Override
