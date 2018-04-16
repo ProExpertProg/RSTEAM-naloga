92122baf2680ba318f5f0287a8ec8ed6eab4f122
Luka Govedic <luka.govedic@gmail.com>
2018-04-16 20:52:56 +0200

    Handling onClick in recyclerView

--- app/src/main/java/com/govedic/luka/rsteam/listGraphics/WordpressPluginsAdapter.java
+++ app/src/main/java/com/govedic/luka/rsteam/listGraphics/WordpressPluginsAdapter.java
@@ -1,5 +1,6 @@
 package com.govedic.luka.rsteam.listGraphics;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
@@ -7,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.govedic.luka.rsteam.PluginDetailsActivity;
@@ -23,14 +26,24 @@ public class WordpressPluginsAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
+        Context context = parent.getContext();
+        View v = LayoutInflater.from(context)
                 .inflate(R.layout.plugin_item_view, parent, false);
-        return new WordpressPluginViewHolder(v);
+        return new WordpressPluginViewHolder(context, v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (mDataset[position] != null) {
+            WordpressPluginViewHolder h = (WordpressPluginViewHolder) holder;
+            ImageView img = h.mItemView.findViewById(R.id.preview);
+            String url = mDataset[position].screenshotURL;
+            if (url != null && !url.isEmpty())
+                Glide.with(h.mContext).load(url).error(R.drawable.ic_launcher_background).centerCrop().into(img);
 
+            TextView name = h.mItemView.findViewById(R.id.pluginName);
+            name.setText(mDataset[position].name);
+        }
     }
 
     @Override
@@ -41,17 +54,20 @@ public class WordpressPluginsAdapter extends RecyclerView.Adapter {
     public class WordpressPluginViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public View mItemView;
+        public Context mContext;
 
-        public WordpressPluginViewHolder(View itemView) {
+        public WordpressPluginViewHolder(Context ctx, View itemView) {
             super(itemView);
             mItemView = itemView;
+            mContext = ctx;
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
             //open the details activity
             int position = getAdapterPosition();
-            if(position != RecyclerView.NO_POSITION) {
+            if (position != RecyclerView.NO_POSITION) {
                 WordpressPlugin plugin = mDataset[position];
                 Intent intent = new Intent(mItemView.getContext(), PluginDetailsActivity.class);
                 intent.putExtra(PluginDetailsActivity.EXTRA_PLUGIN, (Parcelable) plugin);
