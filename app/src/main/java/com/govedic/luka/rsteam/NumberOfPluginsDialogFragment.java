91649c53fddc1f21ed38cfc796564f14beafa2d4
Luka Govedic <luka.govedic@gmail.com>
2018-04-16 22:42:53 +0200

    Number of plugins selected via dialog and saved to shared preferences

---
+++ app/src/main/java/com/govedic/luka/rsteam/NumberOfPluginsDialogFragment.java
@@ -0,0 +1,62 @@
+package com.govedic.luka.rsteam;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+public class NumberOfPluginsDialogFragment extends DialogFragment {
+    public interface ValueChosenListener {
+        public void onValueChosen(int value);
+    }
+
+    // Use this instance of the interface to deliver action events
+    ValueChosenListener mListener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (ValueChosenListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //initialize the number picker
+        final NumberPicker numberPicker = new NumberPicker(getActivity());
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(100);
+
+        // Build the dialog and set up the button click handlers
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.set_num_plugins)
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the positive button event back to the host activity
+                        mListener.onValueChosen(numberPicker.getValue());
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the negative button event back to the host activity
+                        // If "Cancel" was pressed, just use 100
+                        mListener.onValueChosen(100);
+                    }
+                });
+
+        builder.setView(numberPicker);
+        return builder.create();
+    }
+
+}
